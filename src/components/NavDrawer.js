8a7f787bde5bf447377e2a0c9e5c619e7327fb83
xmq291 <xmq291@gmail.com>
2018-10-02 18:37:00 -0400

    nav drawer links

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -4,10 +4,10 @@ import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Menu from 'material-ui/svg-icons/navigation/menu';
-import Template from '../containers/Template';
+import { Link } from 'react-router';
 
 class NavDrawer extends Component {
-    state = { open:false }
+    state = { open:true }
 
     toggle = () => {
         this.setState((prevState, props) => {
@@ -35,14 +35,15 @@ class NavDrawer extends Component {
                         LoginContainer
                     </div>
                     <Divider />
-                    <MenuItem 
-                    primaryText={'Play'}
-                    />
-                    <MenuItem 
-                    primaryText={'Profile'}
-                    />
+                    <Link to={'/'}>
+                        <MenuItem onTouchTap = {this.toggle}
+                        primaryText={'Play'}/>
+                    </Link>
+                    <Link to={'/profile'}>
+                        <MenuItem onTouchTap = {this.toggle}
+                        primaryText={'Profile'}/>
+                    </Link>
                 </Drawer>
-
             </div>
         )
     }
