ab4628adfd96ce6b36f565778e3dae6441c31e7f
xmq291 <xmq291@gmail.com>
2018-10-02 18:00:23 -0400

    building a new drawer

---
+++ src/components/NavDrawer.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Menu from 'material-ui/svg-icons/navigation/menu';
+import Template from '../containers/Template';
+
+class NavDrawer extends Component {
+    render(){
+        return (
+            <div>
+                <FloatingActionButton>
+                    <Menu />
+                </FloatingActionButton>
+                <Drawer>
+                    <Divider />
+                    <MenuItem 
+                    primaryText={'Play'}
+                    />
+                    <MenuItem 
+                    primaryText={'Profile'}
+                    />
+                </Drawer>
+
+            </div>
+        )
+    }
+}
+
+export default NavDrawer;
